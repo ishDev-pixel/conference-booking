bed2e6efeb1981f1f1f23339901e782129e7b469
Ishrat <posgeishrat@gmail.com>
2026-03-02 09:48:10 +0530

    user complete

--- client/src/pages/UserDashboard.jsx
+++ client/src/pages/UserDashboard.jsx
@@ -8,7 +8,9 @@ const UserDashboard = () => {
       <div className='h-screen w-screen bg-red-400'>
        <div className='h-30 w-screen bg-amber-300 p-2'>
         <div className='bg-sky-400 h-25 w-25 rounded-full'></div>
+
       </div>
+      <h2>User Name</h2>
       </div>
      
 
